4abf1dfa2fdfd223cf90d3f1122aa01d9ecdb04d
Trotteriniku <stefanozidda3@gmail.com>
2024-01-20 16:40:49 +0100

    MILESTONE bonus

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,54 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.title }}
+      </li>
+    </ul>
+    <button @click="nextPage()">Avanti</button>
+    <button @click="previousPage()">Dietro</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+import axios from "axios";
+import { store } from "./store"
+  export default {
+    name: "App",
+    data(){
+      return{
+        store,
+        projects:[],
+        currentPage:1,
+        lastPage:0,
+      };
+    },
+    methods: {
+      getAllProjects(){
+        console.log('pippo');
+        axios.get(store.apiUrl + "/projects", {params: {page : this.currentPage}}).then((res)=>{
+          console.log(res);
+          this.projects= res.data.data;
+          this.currentPage = res.data.current_page;
+          console.log(this.currentPage);
+          this.lastPage = res.data.last_page;
+        })
+      },
+      nextPage(){
+        this.currentPage = this.currentPage + 1;
+        this.getAllProjects();
+      },
+      previousPage(){
+        this.currentPage = this.currentPage - 1;
+        this.getAllProjects();
+      }
+    },
+    mounted(){
+      this.getAllProjects();
+    }
   }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style lang="scss" scoped>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
