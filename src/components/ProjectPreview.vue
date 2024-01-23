ecb28ff1e0c8b9a3d5a361959dd08c22fe78f027
Trotteriniku <stefanozidda3@gmail.com>
2024-01-23 09:43:17 +0100

    adding new component

---
+++ src/components/ProjectPreview.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="p-5">
+        cccs
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ProjectPreview",
+        data(){
+            return{
+            }
+        },
+        components:{
+
+        },
+        methods:{
+
+        },
+        mounted(){
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+div{
+    background-color: #0b2531;
+}
+</style>
