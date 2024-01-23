d5fabaa796d9cbcd708d089f34d687f993f3ef1d
Trotteriniku <stefanozidda3@gmail.com>
2024-01-23 09:42:38 +0100

    MILESTONE 3 + bonus

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,66 @@
+<template>
+<nav class="navbar navbar-expand-lg   p-3 ">
+  <a class="navbar-brand text-light display-1 fs-4   " href="#" style="padding-bottom:10px;border-bottom: 2px dotted #e58946; ">Stefano.Z</a>
+  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        
+        <li class="nav-item" v-for="item in menu" >
+            <router-link class="nav-link route-hover "  active-class="my-active"  :to="{ name:item.name}">{{ item.label }}</router-link>
+            <!-- <a class="nav-link" href="#">Link</a> -->
+        </li>
+       
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+  </div>
+</nav>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+        data(){
+            return{
+                menu: [
+                    {
+                        label:"Home",
+                        name: "home",
+
+                    },
+                    {
+                        label:"Projects",
+                        name:"projects"
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+nav{
+    background-color: #34596F;
+    // background-color: #045174;
+    // box-shadow: 2px 1px 20px #3a3a3a6b;
+    height: 70px;
+
+    .route-hover{
+        padding-bottom:11px;
+        color: whitesmoke;
+        &:hover{
+            color: #e58946!important;
+           border-bottom: 2px solid #e58946; 
+        }
+        
+    }
+    .my-active{
+              color: #e58946!important;
+    }
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card">
+        <img :src="`${store.imgPath}${project.preview}`" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ truncateTitle }}</h5>
+            <p class="card-text">{{ truncateBody }}</p>
+            <router-link :to="{ name: 'single-project', params: { slug: project.slug } }" class="btn rounded-0 btn-outline-orange ">
+            View detail
+            </router-link>
+        
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import { store } from "../store";
+export default {
+        name: 'ProjectCard',
+        props: ['project'],
+        data(){
+            return {
+                store,
+                titleMaxLen: 25,
+                bodyMaxLen: 100,
+            }
+        },
+        computed:{
+            truncateTitle(){
+                if(this.project.title.length > this.titleMaxLen){
+                    return this.project.title.substr(0,this.titleMaxLen) + '...';
+                }
+                return this.project.title;            
+            },
+            truncateBody(){
+                if(this.project.body.length > this.bodyMaxLen){
+                    return this.project.body.substr(0,this.bodyMaxLen) + '...';
+                }
+                return this.project.body;            
+            }
+        }
+        
+}
+</script>
+
+<style lang="scss" scoped>
+.card{
+    min-height: 200px;
+}
+</style>
